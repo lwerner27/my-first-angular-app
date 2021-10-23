82e23def1b1615db7c6f8e75f6d917e893a71172
Lucas Werner <lwerner27@gmail.com>
2021-10-22 23:18:08 -0500

    Added methods and data binding to the servers component.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer: boolean = false;
+  newServerName: string = ""
+  serverCreationStatus = "No server was created."
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'A new server was ceated with the name: ' + this.newServerName;
+  }
+
+  // We don't necessarily need this method anymore because in our template we are using 2 way data binding
+  onUpdateServerName(event: Event) {
+    this.newServerName = (<HTMLInputElement>event.target).value;
+
   }
 
 }
